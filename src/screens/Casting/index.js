9d0a0ebe9efb0c831d23e57143e3f08a881d3903
AreebVohra <areebvohra@gmail.com>
2019-07-07 23:05:40 +0500

    --commit: Casting screen created

---
+++ src/screens/Casting/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+
+export default class CastingScreen extends Component {
+    render() {
+        return (
+            <View></View>
+        );
+    }
+}
