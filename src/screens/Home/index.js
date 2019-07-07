e0c36840014cac144d1db8c8515b3dccc4698cd3
AreebVohra <areebvohra@gmail.com>
2019-07-07 23:05:54 +0500

    --commit: Home screen created

---
+++ src/screens/Home/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+
+export default class HomeScreen extends Component {
+    render() {
+        return (
+            <View></View>
+        );
+    }
+}
