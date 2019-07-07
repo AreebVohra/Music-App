eba4f107d7cec38664e2429e4b5cfc1522f81fcc
AreebVohra <areebvohra@gmail.com>
2019-07-07 23:03:39 +0500

    --Commit: Input with label component created

---
+++ src/components/InputWithLabel.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, TextInput, Text } from 'react-native';
+
+const InputWithLabel = (props) => {
+    const {label} = props;
+    return (
+        <View style={{ flex: 1, paddingLeft: 30, alignSelf: 'stretch', alignItems: 'flex-start', }} >
+            <Text style={{ marginLeft: 10 }}>{label}</Text>
+            <TextInput style={{
+                borderBottomWidth: 1,
+                borderBottomColor: 'gray',
+                width: '85%',
+                marginLeft: 10,
+                fontSize: 20
+            }} />
+        </View>
+    )
+}
+
+export default InputWithLabel;
