e40221d756fdf8766248d3f79cac91835bad66ed
AreebVohra <areebvohra@gmail.com>
2019-07-18 21:13:04 +0500

    --commit: Footer Added

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -1,19 +1,53 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Image, StatusBar, Dimensions, Text } from 'react-native';
+import HeaderText from '../../components/HeaderText';
+import InputWithLabel from '../../components/InputWithLabel';
+import Footer from '../../components/footer';
 
 
 export default class SignupScreen extends Component {
     render() {
+        StatusBar.setHidden(true);
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row' }}>
-                    <View style={{ flex: 1, alignSelf: 'stretch' }} />
-                    <View style={{ flex: 1, alignSelf: 'stretch' }} />
-                    <View style={{ flex: 1, alignSelf: 'stretch' }} />
-                    <View style={{ flex: 1, alignSelf: 'stretch' }} />
-                    <View style={{ flex: 1, alignSelf: 'stretch' }} />
+            <View style={styles.mainView}>
+                <Image style={styles.topImage} source={require('../../assets/images/top.png')} />
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'column' }} />
+
+                    <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'column' }} />
+                    <View style={{ flex: 1, alignSelf: 'stretch', alignItems: 'center', justifyContent: 'center' }} >
+                        <HeaderText title="Sign Up" />
+                    </View>
+                    <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'column' }} />
+                    <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'column' }}>
+                        <InputWithLabel label="NAME" />
+                    </View>
+                    <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'column' }}>
+                        <InputWithLabel label="PASSWORD" />
+                    </View>
+                    <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'column' }}>
+                        <InputWithLabel label="CITY" />
+                    </View>
+                    <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'column' }}>
+                        <InputWithLabel label="BIRTHDAY" />
+                    </View>
+                    <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'column' }} />
+                    <Footer Nextlink="SignIn"/>
+                    <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'column' }} />
                 </View>
-            </View>
+            </View >
         );
     }
-}   
+}
+
+const styles = {
+    mainView: {
+        flex: 1,
+    },
+    topImage: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+        position: 'absolute',
+        zIndex: -1
+    }
+}
