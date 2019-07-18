aa747b37992310c7d23c72494d5d73a2f65f308f
AreebVohra <areebvohra@gmail.com>
2019-07-18 21:13:20 +0500

    --commit: footer comtainer created

---
+++ src/components/footer.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+import { AntDesign } from "@expo/vector-icons";
+
+
+const Footer = (props) => {
+    const { Nextlink } = props
+    return (
+        <View style={styles.mainView}>
+            <View style={styles.cells} >
+                <Image source={require('../assets/images/facebookIcon.png')} />
+            </View>
+            <View style={styles.cells} >
+                <Image source={require('../assets/images/googleIcon.png')} />
+            </View>
+            <View style={styles.cells} >
+                <Image source={require('../assets/images/twitterIcon.png')} />
+            </View>
+            <View style={{ flex: 3, alignItems: 'flex-end' }} >
+                <Text
+                    onPress={() => this.navigation.navigate(Nextlink)}
+                    style={styles.nextText}>
+                    Next <AntDesign name="right" size={18} color='#95989A'
+                    />
+                </Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = {
+    mainView: {
+        flex: 1,
+        alignSelf: 'stretch',
+        flexDirection: 'row',
+        paddingLeft: 40,
+        paddingRight: 40
+    },
+    cells: {
+        flex: 1,
+        alignSelf: 'stretch'
+    },
+    nextText: {
+        color: '#95989A',
+        fontSize: 22,
+        fontFamily: 'sans-serif'
+    }
+}
+
+export default Footer;
